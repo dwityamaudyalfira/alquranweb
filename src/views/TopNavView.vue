05ed8035c42947228c5329f3c72b43b9e5526e7e
dwityamaudyalfira <76533748+dwityamaudyalfira@users.noreply.github.com>
2022-05-09 21:48:59 +0700

    commit

--- src/views/TopNavView.vue
+++ src/views/TopNavView.vue
@@ -1,17 +1,34 @@
 <template>
-  <nav class="navbar navbar-light" style="background-image: url('src/assets/bgnavbar.png');">
+  <nav
+    class="navbar navbar-light"
+    style="background-image: url('src/assets/bgnavbar.png')"
+  >
     <div class="container-fluid">
       <a class="navbar-brand" href="/surah">
-        <img src="src/assets/logo2.png" alt="" width="100" height="100" class="d-inline-block align-text-middle">
+        <img
+          src="src/assets/logo2.png"
+          alt=""
+          class="d-inline-block align-text-middle"
+        />
         Quran.com by Dwitya Maudy Alfira
       </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarText"
+        aria-controls="navbarText"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <RouterLink class="nav-link active" aria-current="page" to="/">Home</RouterLink>
+            <RouterLink class="nav-link active" aria-current="page" to="/"
+              >Home</RouterLink
+            >
           </li>
           <li class="nav-item">
             <RouterLink class="nav-link" to="/surah">Baca Al-Qur'an</RouterLink>
@@ -29,20 +46,23 @@
 </template>
 
 <script>
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
   // Check if there are any navbar burgers
   if ($navbarBurgers.length > 0) {
     // Add a click event on each of them
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
+    $navbarBurgers.forEach((el) => {
+      el.addEventListener("click", () => {
         // Get the target from the "data-target" attribute
         const target = el.dataset.target;
         const $target = document.getElementById(target);
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
+        el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
       });
     });
   }
@@ -50,12 +70,12 @@ document.addEventListener('DOMContentLoaded', () => {
 </script>
 
 <style scoped>
-.navbar-brand{
+.navbar-brand {
   display: block;
   margin-left: auto;
   margin-right: auto;
 }
-.navbar-light{
+.navbar-light {
   background-size: cover;
 }
 </style>
